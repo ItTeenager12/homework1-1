9aadcfb0e890f1e2f79bc88a2d16af7a9d97fd67
Alex <oleksejbogomolov@gmail.com>
2025-07-24 22:34:13 +0300

    HomeWork2

--- homework.js
+++ homework.js
@@ -1,22 +1,4 @@
-let num = 10369;
-let d1 = num % 10;
-num = Math.floor(num / 10);
-
-
-let d2 = num % 10;
-num = Math.floor(num / 10);
-
-
-
-let d3 = num % 10;
-num = Math.floor(num / 10);
-
-
-let d4 = num % 10;
-num = Math.floor(num / 10);
-
-
-let d5 = num % 10;
-num = Math.floor(num / 10);
-
-console.log(d1,d2,d3,d4,d5);
+let string1 = prompt("hello");
+let string2 = prompt("bye");
+let string3 = prompt("World");
+console.log(string3 + " ",  string2 + " ",  string1 +  " " );
