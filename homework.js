32cf11ab08f46719d1febd46df02163e2f2755b4
Alex <oleksejbogomolov@gmail.com>
2025-07-24 22:26:02 +0300

    myHomeWork

---
+++ homework.js
@@ -0,0 +1,22 @@
+let num = 10369;
+let d1 = num % 10;
+num = Math.floor(num / 10);
+
+
+let d2 = num % 10;
+num = Math.floor(num / 10);
+
+
+
+let d3 = num % 10;
+num = Math.floor(num / 10);
+
+
+let d4 = num % 10;
+num = Math.floor(num / 10);
+
+
+let d5 = num % 10;
+num = Math.floor(num / 10);
+
+console.log(d1,d2,d3,d4,d5);
