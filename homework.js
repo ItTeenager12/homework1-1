ea8fd41c1ad9b61cd24b7e2275678ff4adbb09b3
ItTeenager12 <oleksejbogomolov@gmail.com>
2025-07-25 14:56:54 +0200

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,4 +1,22 @@
-let string1 = prompt("hello");
-let string2 = prompt("bye");
-let string3 = prompt("World");
-console.log(string3 + " ",  string2 + " ",  string1 +  " " );
+let num = 10369;
+let d1 = num % 10;
+num = Math.floor(num / 10);
+
+
+let d2 = num % 10;
+num = Math.floor(num / 10);
+
+
+
+let d3 = num % 10;
+num = Math.floor(num / 10);
+
+
+let d4 = num % 10;
+num = Math.floor(num / 10);
+
+
+let d5 = num % 10;
+
+
+console.log(d5, d4, d3, d2, d1);
